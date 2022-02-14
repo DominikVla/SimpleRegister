5f443838fb06eb705b380a0f232b3c723b5220a3
Dominik <74500880+DominikVla@users.noreply.github.com>
2022-02-14 09:23:46 +0000

    Add files via upload

---
+++ Register/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="css/register.css">
+</head>
+<body>
+
+<form action="/action_page.php">
+  <div class="container">
+    <h1>Register v1.0</h1>
+    <p>Please fill in this form to create an account.</p>
+    <hr>
+
+    <label for="email"><b>Email</b></label>
+    <input type="text" placeholder="Enter Email" name="email" id="email" required>
+
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" id="psw" required>
+
+    <label for="psw-repeat"><b>Repeat Password</b></label>
+    <input type="password" placeholder="Repeat Password" name="psw-repeat" id="psw-repeat" required>
+    <hr>
+    
+    <button type="submit" class="registerbtn">Register</button>
+  </div>
+</body>
+</html>
